20879a7cf510c2983c6e079ce1c7405b07beaa3c
voidobserve <99012263+voidobserve@users.noreply.github.com>
2025-11-06 16:46:06 +0800

    2025-11-06-周四，CRC-EA65-0F6142CF

---
+++ apps/spp_and_le/board/bd29/board_config.h
@@ -0,0 +1,19 @@
+#ifndef BOARD_CONFIG_H
+#define BOARD_CONFIG_H
+
+/*
+ *  板级配置选择
+ */
+#define CONFIG_BOARD_AC631N_DEMO
+// #define CONFIG_BOARD_AC6311_DEMO
+// #define CONFIG_BOARD_AC6313_DEMO
+// #define CONFIG_BOARD_AC6318_DEMO
+// #define CONFIG_BOARD_AC6319_DEMO
+
+#include "board_ac631n_demo_cfg.h"
+#include "board_ac6311_demo_cfg.h"
+#include "board_ac6313_demo_cfg.h"
+#include "board_ac6318_demo_cfg.h"
+#include "board_ac6319_demo_cfg.h"
+
+#endif
